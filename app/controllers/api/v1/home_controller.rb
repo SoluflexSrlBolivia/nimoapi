652884be4936892de6dc5cb094b8448e5db00cb2
Nelson Garcia <nelsongarcia.info@gmail.com>
2016-11-17 22:35:36 -0400

    fix query

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -5,44 +5,37 @@ class Api::V1::HomeController < Api::V1::BaseController
   def index
   	group_ids = current_user.group_ids.join(',')
 
-    recently_archives = []
-    recently_posts = []
     unless group_ids.empty?
       recently_archives =Archive.where("owner_type = 'Group' AND owner_id IN (#{group_ids})").order(created_at: :desc)
       recently_posts = Post.where("group_id IN (#{group_ids})").order(created_at: :desc)
+
+      recently_archives = apply_filters(recently_archives, params)
+      recently_archives = paginate(recently_archives)
+
+      recently_posts = apply_filters(recently_posts, params)
+      recently_posts = paginate(recently_posts)
+
+      archives = ActiveModel::ArraySerializer.new(
+          recently_archives,
+          each_serializer: Api::V1::HomeArchiveSerializer,
+          root: "archives",
+          meta: meta_attributes(recently_archives)
+      )
+      posts = ActiveModel::ArraySerializer.new(
+          recently_posts,
+          each_serializer: Api::V1::HomePostSerializer,
+          root: "posts",
+          meta: meta_attributes(recently_posts)
+      )
+
+      recents = Recent.new(posts, archives)
+
+      return render(
+          json: recents
+      )
     end
 
-    archives = []
-    unless recently_archives.empty?
-      archives = apply_filters(recently_archives, params)
-      archives = paginate(archives)
-    end
-
-    posts = []
-    unless recently_posts.empty?
-      posts = apply_filters(recently_posts, params)
-      posts = paginate(posts)
-    end
-
-    archives = ActiveModel::ArraySerializer.new(
-        archives,
-        each_serializer: Api::V1::HomeArchiveSerializer,
-        root: "archives",
-        meta: meta_attributes(archives)
-    )
-    posts = ActiveModel::ArraySerializer.new(
-        posts,
-        each_serializer: Api::V1::HomePostSerializer,
-        root: "posts",
-        meta: meta_attributes(posts)
-    )
-
-    recents = Recent.new(posts, archives)
-
-    render(
-      json: recents
-    )
-
+    render json: {:posts=>[], :archives=>[]}
   end
 
   private
