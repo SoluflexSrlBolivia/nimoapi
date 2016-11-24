21af9c70ca304798ed616b0d764601783d52c8c5
Nelson Garcia <nelsongarcia.info@gmail.com>
2016-11-24 17:01:32 -0400

    add group param with pagination

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -10,13 +10,17 @@ class Api::V1::GroupsController < Api::V1::BaseController
     end
   end
 
+  def_param_group :paginate do
+    param :locale, String, :desc => "language", :required => false
+    param :page, String, :desc => "# de pagina", :required => false
+    param :per_page, String, :desc => "# de registros por pagina", :required => false
+  end
+
 
   #########Files
   api! "listado de fotos de un group"
   param :id, Fixnum, :desc => "ID Group",  :required => true
-  param :locale, String, :desc => "language", :required => false
-  param :page, String, :desc => "# de pagina", :required => false
-  param :per_page, String, :desc => "# de registros por pagina", :required => false
+  param_group :paginate
   meta :header => "Authorization:Token token=pU7SOyDNY+URPeGZHlE/knqWzv131oTPOf/t3aXs+mM5x0zGrQfbi+5lGasQl47A6HaLTaPNUbN9KJQ2hA7QYw==, email=demo@gmail.com"
   error 401, "Bad credentials"
   error 403, "not authorized"
