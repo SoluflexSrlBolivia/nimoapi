e2d6a4f665c1d81e61bcfe093936c4618f2804a0
Nelson Garcia <nelsongarcia.info@gmail.com>
2016-12-01 14:52:59 -0400

    add gender on list of members

--- app/serializers/api/v1/user_search_serializer.rb
+++ app/serializers/api/v1/user_search_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::UserSearchSerializer < Api::V1::BaseSerializer
-  attributes :id, :email, :fullname, :name, :lastname, :phone_number
+  attributes :id, :email, :fullname, :name, :lastname, :phone_number, :gender
 
   def fullname
   	object.try(:fullname) || "Sin nombre"
