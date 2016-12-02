84fc3baf1416fc79539111f45ab0ea574bfabc15
Nelson Garcia <nelsongarcia.info@gmail.com>
2016-12-02 18:17:45 -0400

    fix send parameters

--- app/workers/new_comment_worker.rb
+++ app/workers/new_comment_worker.rb
@@ -1,14 +1,13 @@
 class NewCommentWorker
   include Sidekiq::Worker
 
-  def perform(notification_message, devices, post)
+  def perform(notification_message, devices, post_id)
     # Do something later
-
-    group = Group.find_by_id post.group_id
+    post = Post.find_by_id post_id
     result = Notification::send_notification notification_message, devices, {
         :type => Notification::NOTIFICATION_NEW_POST,
         :message => notification_message,
-        :group_id => group.id,
+        :group_id => post.group.id,
         :post=>Api::V1::HomePostSerializer.new(post, root: false)
     }
 
