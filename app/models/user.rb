ea3bc908a96abaee42fb5d9aafa7e475a3f639ed
Nelson Garcia <nelsongarcia.info@gmail.com>
2017-01-26 19:42:32 -0400

    test rule

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   #validates :name,  presence: true, length: { maximum: 50 }
   validates :name,  length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
-  VALID_PASSWORD_REGEX = /\A(?=\.*?[A-Z]).{8,}/i
+  VALID_PASSWORD_REGEX = /\A(?=\.*?[A-Z]){8,}$/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX, message: I18n.t(:error_email_format) },
                     uniqueness: { case_sensitive: false, message: I18n.t(:error_email_already_exist) }
