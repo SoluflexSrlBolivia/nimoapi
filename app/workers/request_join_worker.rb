bcd95b0bb4e6e3e539c4814ae8a6ebb8408190b0
Nelson Garcia <nelsongarcia.info@gmail.com>
2017-01-31 09:43:18 -0400

    restore join worker and add sidekiq config

--- app/workers/request_join_worker.rb
+++ app/workers/request_join_worker.rb
@@ -5,7 +5,11 @@ class RequestJoinWorker
 
     result = Notification::send_notification notification_message, devices, {
         :type => Notification::NOTIFICATION_REQUEST_TO_JOIN_GROUP,
-        :message => notification_message
+        :message => notification_message,
+        :notification => {:id => notification_id,
+                        :notification_type => Notification::NOTIFICATION_REQUEST_TO_JOIN_GROUP,
+                        :action => notification_action
+        }
     }
 
     puts "request_to_join:#{result}"
