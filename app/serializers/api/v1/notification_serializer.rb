e9bffc085a8e4e1b4f9651e5c28e2575b8a29840
Nelson Garcia <nelsongarcia.info@gmail.com>
2017-01-30 21:35:21 -0400

    test notification_type

--- app/serializers/api/v1/notification_serializer.rb
+++ app/serializers/api/v1/notification_serializer.rb
@@ -1,5 +1,8 @@
 class Api::V1::NotificationSerializer < Api::V1::BaseSerializer
   attributes :id, :message, :notification_type, :action
-  
+
+  def notification_type
+    object.try(:notification_type)
+  end
   
 end
