38d355ad38544697ae109ae99c8258c6695dac19
Nelson Garcia <nelsongarcia.info@gmail.com>
2017-02-02 10:37:42 -0400

    upgrade messages and reset validation of no actives account

--- app/controllers/api/v1/password_resets_controller.rb
+++ app/controllers/api/v1/password_resets_controller.rb
@@ -5,12 +5,19 @@ class Api::V1::PasswordResetsController < Api::V1::BaseController
 	def create
     user = User.find_by(email: user_params[:email].downcase)
     if user
-      user.create_reset_digest
-      user.send_password_reset_email
-      render(
-        json: {:status=>"ok", :message=>t(:email_instruction_change_password)},
-        status: 201
-      )
+      if user.activated?
+        user.create_reset_digest
+        user.send_password_reset_email
+        render(
+            json: {:status=>"ok", :message=>t(:email_instruction_change_password)},
+            status: 201
+        )
+      else
+        render(
+            json: {:status=>"fail", :message=>t(:account_not_active)},
+            status: 201
+        )
+      end
     else
       render(
         json: {:status=>"fail", :message=>t(:email_not_found)},
