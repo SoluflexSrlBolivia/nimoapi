2205e8e758b1aeb654b66af23d56a2994980289b
Nelson Garcia <nelsongarcia.info@gmail.com>
2017-02-03 22:56:26 -0400

    update policy of folders

--- app/policies/folder_policy.rb
+++ app/policies/folder_policy.rb
@@ -13,12 +13,16 @@ class FolderPolicy < ApplicationPolicy
 
   def update?
     return true if user.admin?
-    return true if record.owner_id == user.id
+    return true if record.owner_type == "User" && record.owner_id == user.id
+    if record.owner_type == "Group"
+      member = UserGroup.find_by(:user_id=>user.id, :group_id=>record.owner.id)
+      return true unless member.nil?
+    end
   end
 
   def destroy?
     return true if user.admin?
-    return true if record.owner_id == user.id
+    return true if record.owner_type == "Group" && record.owner_id == user.id
   end
 
   class Scope < ApplicationPolicy::Scope
